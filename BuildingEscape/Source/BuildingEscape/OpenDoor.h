47b9447902a0912c3f66d11a869824d67ef7c4f3
Mat Jennings <mat.jennings@appian.com>
2018-02-13 17:41:13 -0500

    Add TriggerVolume that opens the Door

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include <Engine/TriggerVolume.h>
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "OpenDoor.generated.h"
@@ -15,15 +16,21 @@ class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
 public:	
 	// Sets default values for this component's properties
 	UOpenDoor();
+    // Called every frame
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
-	
+private:
+    UPROPERTY(VisibleAnywhere)
+    float OpenAngle = 90.0f;
+    
+    UPROPERTY(EditAnywhere)
+    ATriggerVolume* PressurePlate;
+    
+    AActor* ActorThatOpens;
+    
+    void OpenDoor();
 };
