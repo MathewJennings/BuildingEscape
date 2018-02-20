e3725b88590baab575e44540d98521007e11b4af
Mat Jennings <mat.jennings@appian.com>
2018-02-20 18:03:29 -0500

    Add Ray Casting to Grabber

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -2,6 +2,8 @@
 
 #include "Grabber.h"
 
+#define OUT
+
 DEFINE_LOG_CATEGORY(GrabberLog);
 
 // Sets default values for this component's properties
@@ -29,7 +31,32 @@ void UGrabber::BeginPlay()
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    
+    /// Get player viewpoint this tick.
+    FVector PlayerViewPointLocation;
+    FRotator PlayerViewPointRotation;
+    GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+        OUT PlayerViewPointLocation,
+        OUT PlayerViewPointRotation
+    );
+    
+    /// Draw a red trace in the world to visualize
+    FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+    DrawDebugLine(GetWorld(), PlayerViewPointLocation, LineTraceEnd, FColor(255, 0, 0), false, 0.0f, 0.0f,  10.0f);
 
-	// ...
+    /// Setup query params
+    FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+
+    /// Ray-cast out to reach distance.
+    FHitResult Hit;
+    GetWorld()->LineTraceSingleByObjectType(OUT Hit, PlayerViewPointLocation, LineTraceEnd, FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody), TraceParams);
+
+    /// See what we hit.
+    AActor* HitActor = Hit.GetActor();
+    if (HitActor)
+    {
+        FString HitActorName = HitActor->GetName();
+        UE_LOG(GrabberLog, Warning, TEXT("Hit: %s"), *HitActorName);
+    }
 }
 
--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Runtime/Engine/Classes/Engine/EngineTypes.h"
+#include "DrawDebugHelpers.h"
 #include "Grabber.generated.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(GrabberLog, Log, All);
@@ -16,15 +18,15 @@ class BUILDINGESCAPE_API UGrabber : public UActorComponent
 public:	
 	// Sets default values for this component's properties
 	UGrabber();
+    
+    // Called every frame
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
-	
+private:
+    // How far ahead of the player we can reach in cm
+    float Reach = 100.0f;
 };
